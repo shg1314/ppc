ca2d3b7a8760162cfa9c5e592ef01e2b8990eacc
siandyChun <siandy.chun@icloud.com>
2018-07-25 12:56:33 +0900

    modified

--- app.js
+++ app.js
@@ -46,6 +46,3 @@ app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
-
-
-  //image page
