7b34a3cfd39f1542d63e3ee30301cfb35813c7cb
siandyChun <siandy.chun@icloud.com>
2018-07-25 19:50:33 +0900

    modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '프라임조달컨설팅' });
+  res.render('index', { title: 'Prime Procurement' });
 });
 
 module.exports = router;
