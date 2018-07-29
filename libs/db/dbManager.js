6b1e6396b54782e6f30cca0bea6ba50e7123f8ea
ggon <bluekain@naver.com>
2018-07-29 13:11:48 +0900

    db 연결 라이브러리 방식 변경 기존방식이 느림

--- libs/db/dbManager.js
+++ libs/db/dbManager.js
@@ -1,30 +1,28 @@
 'use strict'
 
-var mysql = require('MySQL');
-
-function DBManager(sqlconnectionString)
-{
-    this.pool = mysql.createPool(sqlconnectionString);
+const mysql = require('MySQL');
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err)
+                    return reject(err);
+                resolve();
+            });
+        });
+    }
 }
-
-DBManager.prototype.connect = function(){
-    this.pool.getConnection(function(err,connection){
-        if(err){
-            if(connection) 
-                connection.release();
-               throw err;
-           }
-    });
-};
-
-
-DBManager.prototype.query = function (sqlquery, arrVal,fn ){
-    this.pool.query(sql,arrVal,fn);
-};
-
-DBManager.prototype.close = function (){
-     if(this.connection) 
-         this.connection.release();
-};
 
 module.exports = DBManager;
